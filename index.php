6a3151db28fed2c2370170e419c7ed2a19a14aa6
sue142857 <sue142857@gmail.com>
2015-12-24 19:08:34 -0800

    new video with highlight.

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
 				The following video shows an example of how to do labeling/matching tasks:
 			</p>
 			<div id='instruction-video'>
-				<embed src="http://player.youku.com/player.php/sid/XMTQxNjU0Nzg1Mg==/v.swf"
+				<embed src="http://player.youku.com/player.php/sid/XMTQyMzM1NzU4NA==/v.swf"
 					   allowFullScreen="true" quality="high" width="600" height="500" align="middle"
 					   allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
 			</div>
